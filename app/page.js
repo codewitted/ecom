0b90b412aaa8150232f91212ec5b5a8de356ab56
Kodi <117670210+codewitted@users.noreply.github.com>
2024-11-19 13:17:14 +0000

    page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import EcommerceApp from '../components/EcommerceApp';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <EcommerceApp />
+    </main>
+  );
+}
